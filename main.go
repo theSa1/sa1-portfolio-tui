f573e6c9f88128e715f717c1b41010d53f5a1b38
Sa1 <i@sa1.dev>
2025-07-30 14:23:11 +0530

    changed error log

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ var port = func() string {
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Debug("No .env file found, using default environment variables")
 	}
 
 	s, err := wish.NewServer(
